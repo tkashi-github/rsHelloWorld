f0bfa438f00c728a1909d77105acdeb3cc66a7e6
Takashi Kashiwagi <tkashi.github@outlook.jp>
2019-06-15 16:01:16 +0900

    if, for

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    let mut cnt: u32 = 0;
+    //let mut _i:u32 = 0;
     println!("Hello, world!");
+
+    for _i in 0..100{
+        if 0 == (cnt % 2) {
+            println!("even {}", cnt);
+        }
+        else
+        {
+            println!("odd {}", cnt);
+        }
+        cnt += 1;
+    }
 }
